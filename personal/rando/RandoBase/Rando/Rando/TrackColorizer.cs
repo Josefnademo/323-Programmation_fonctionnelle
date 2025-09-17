6d436d51a7aeb3983515efc3291773ad62e0ca4b
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-09-17 11:46:11 +0200

    personal\rando(rando(trackpoints,))[WIP]

---
+++ personal/rando/RandoBase/Rando/Rando/TrackColorizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Rando
+{ // Class to assign colors to track points based on elevation
+    internal class TrackColorizer
+    {
+    
+    }
+}
---
+++ personal/rando/RandoBase/Rando/Rando/Trackpoints.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+
+namespace Rando
+{
+    // Class representing a single GPS track point
+    public class Trackpoint
+    {
+        public double Latitude { get; set; }   // Latitude in decimal degrees
+        public double Longitude { get; set; }  // Longitude in decimal degrees
+        public double Elevation { get; set; }  // Elevation in meters
+    }
+
+
+    // Class to read GPX files and return a list of Trackpoints
+    public class GPXReader
+    {
+        // Reads a GPX file and parses all track points
+        public static List<Trackpoint> ReadGPX(string filePath)
+        {
+            List<Trackpoint> points = new List<Trackpoint>();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath); // Load GPX XML file
+
+            XmlNodeList trkpts = doc.GetElementsByTagName("trkpt"); // Find all <trkpt> nodes
+
+            foreach (XmlNode node in trkpts)
+            {//chatgpt help to make syntazx and correctly Parse data
+
+                // Parse latitude and longitude from attributes
+                double lat = double.Parse(node.Attributes["lat"].Value, System.Globalization.CultureInfo.InvariantCulture);
+                double lon = double.Parse(node.Attributes["lon"].Value, System.Globalization.CultureInfo.InvariantCulture);
+
+                // Parse elevation from child <ele> element
+                double ele = double.Parse(node["ele"].InnerText, System.Globalization.CultureInfo.InvariantCulture);
+
+                // Add a new Trackpoint to the list
+                points.Add(new Trackpoint
+                {
+                    Latitude = lat,
+                    Longitude = lon,
+                    Elevation = ele
+                });
+            }
+
+            return points; // Return all parsed track points
+        }
+    }
+}
